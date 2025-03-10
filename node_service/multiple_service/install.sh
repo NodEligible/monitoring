4c894b5e99a26998cb2d9a45b9ad74e52030ad77
NodEligible <dudarvasya1999@gmail.com>
2025-03-10 23:43:33 +0100

    Update install.sh

--- node_service/multiple_service/install.sh
+++ node_service/multiple_service/install.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+curl -s https://raw.githubusercontent.com/NodEligible/programs/refs/heads/main/display_logo.sh | bash
+
+YELLOW='\e[0;33m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
 CONFIG_FILE="/etc/multiple-healthcheck.conf"
 
 # Запитуємо у користувача дані
-read -p "Enter IDENTIFIER: " IDENTIFIER
-read -p "Enter PIN: " PIN
+echo -e "${YELLOW}Введите IDENTIFIER: ${NC}"
+read IDENTIFIER
+echo -e "${YELLOW}Введите PIN: ${NC}"
+read PIN
 
 # Зберігаємо дані у файл
 echo "IDENTIFIER=$IDENTIFIER" | sudo tee $CONFIG_FILE > /dev/null
 echo "PIN=$PIN" | sudo tee -a $CONFIG_FILE > /dev/null
+if [[ -z "$IDENTIFIER" || -z "$PIN" ]]; then
+    echo -e "${RED}❌ Ошибка: IDENTIFIER или PIN не могут быть пустыми!${NC}"
+    exit 1
+fi
 
 # Дозволяємо читання тільки root
 sudo chmod 600 $CONFIG_FILE
