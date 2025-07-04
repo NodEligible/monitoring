f62ea44aecfeaee67e0afaff723251c1a280f8a6
NodEligible <dudarvasya1999@gmail.com>
2025-07-04 16:42:49 +0200

    Update gaianet.sh

--- node_service/gaianet.sh
+++ node_service/gaianet.sh
@@ -8,11 +8,11 @@ NC='\033[0m'
 
 
 echo -e "${YELLOW} Удаляем сервис если есть...${NC}"
-sudo systemctl stop gaianet-monitor
-sudo systemctl disable gaianet-monitor
-systemctl daemon-reload
-rm -rf /root/gaianet_service
-rm -rf /etc/systemd/system/gaianet-monitor.service
+sudo systemctl stop gaianet-monitor &>/dev/null
+sudo systemctl disable gaianet-monitor &>/dev/null
+systemctl daemon-reload 
+rm -rf /root/gaianet_service &>/dev/null
+rm -rf /etc/systemd/system/gaianet-monitor.service &>/dev/null
 
 INSTALL_DIR="/root/gaianet_service"
 SERVICE_NAME="gaianet-monitor"
